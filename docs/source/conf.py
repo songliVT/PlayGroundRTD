4cbcbd80c240f2fe4581ff15d618cd0a7b68a444
songliVT <songli@vt.edu>
2025-12-15 09:24:20 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PlantSCHub'
+copyright = '2025, Song Li Lab at Virginia Tech'
+author = 'Song Li'
 
 release = '0.1'
 version = '0.1.0'
